406531f7adcb5e28c544eaebe1f57fa97119878b
mingyu <tnt01010@naver.com>
2023-12-16 15:57:34 +0900

    231216

---
+++ python/10768.py
@@ -0,0 +1,16 @@
+"""
+특별한 날
+"""
+a = int(input())
+b = int(input())
+if a < 2:
+    print('Before')
+elif a == 2:
+    if b < 18:
+        print('Before')
+    elif b == 18:
+        print('Special')
+    else:
+        print('After')
+else:
+    print('After')
