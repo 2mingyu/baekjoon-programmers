0afdf1757cc170906010b72c0254f56a43d48f2e
mingyu <tnt01010@naver.com>
2024-02-02 21:24:43 +0900

    240202

---
+++ python/4299.py
@@ -0,0 +1,11 @@
+"""
+AFC 윔블던
+"""
+a, b = map(int, input().split())
+if a < b: print(-1)
+else:
+    c = (a+b) // 2
+    d = (a-b) // 2
+    if c+d == a and c-d == b:
+        print(c, d)
+    else: print(-1)
