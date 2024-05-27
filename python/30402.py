78f92cbf7fb18073edc29c97f028577b62ea3038
2mingyu <tnt01010@naver.com>
2024-05-27 14:33:25 +0900

    240527

---
+++ python/30402.py
@@ -0,0 +1,14 @@
+"""
+감마선을 맞은 컴퓨터
+"""
+for _ in range(15):
+    l = input()
+    if 'w' in l:
+        print('chunbae')
+        break
+    elif 'b' in l:
+        print('nabi')
+        break
+    elif 'g' in l:
+        print('yeongcheol')
+        break
