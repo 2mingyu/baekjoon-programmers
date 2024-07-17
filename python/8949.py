766863c783fce4e92e282ce23de09c5707b1264a
2mingyu <tnt01010@naver.com>
2024-07-17 16:03:47 +0900

    240717

---
+++ python/8949.py
@@ -0,0 +1,7 @@
+"""
+대충 더해
+"""
+A, B = input().split()
+if len(A) > len(B): B = '0' * (len(A) - len(B)) + B
+else: A = '0' * (len(B) - len(A)) + A
+for i in range(len(A)): print(int(A[i]) + int(B[i]), end="")
