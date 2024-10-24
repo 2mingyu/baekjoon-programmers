bbfe74ea0de7371ddca2ab12ab51e9b785206928
2mingyu <76238096+2mingyu@users.noreply.github.com>
2024-10-24 19:06:31 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, letter) {
+    var answer = '';
+    for (c of my_string) {
+        if (c != letter) {
+            answer += c;
+        }
+    }
+    return answer;
+}
