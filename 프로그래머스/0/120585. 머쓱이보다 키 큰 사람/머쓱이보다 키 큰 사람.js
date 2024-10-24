a979e479f8549cd9f2f0c122031e2aca562b90f7
2mingyu <76238096+2mingyu@users.noreply.github.com>
2024-10-24 18:50:33 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,9 @@
+function solution(array, height) {
+    var answer = 0;
+    for (number of array) {
+        if (number > height) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
