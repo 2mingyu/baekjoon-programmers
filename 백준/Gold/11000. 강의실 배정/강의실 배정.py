31191e815cd8f76ff93c91e8b70053e19017f970
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-01-24 22:04:17 +0900

    [Gold V] Title: 강의실 배정, Time: 364 ms, Memory: 76548 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+l = sorted([list(map(int, input().split())) for _ in range(N)])
+q = []
+heapq.heappush(q, l[0][1])
+for i in range(1, N):
+    if q[0] <= l[i][0]:
+        heapq.heappushpop(q, l[i][1])
+    else:
+        heapq.heappush(q, l[i][1])
+print(len(q))
