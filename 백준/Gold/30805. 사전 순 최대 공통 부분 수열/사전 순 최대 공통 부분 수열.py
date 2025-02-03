5b834aee62156afa3bd684a281eb06c3be4619de
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-02-03 17:17:14 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
@@ -0,0 +1,16 @@
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+C = []
+i = 100
+while i > 0 and A and B:
+    if i in A and i in B:
+        C.append(i)
+        A = A[A.index(i)+1:]
+        B = B[B.index(i)+1:]
+        i = 100
+    else:
+        i -= 1
+print(len(C))
+if len(C): print(*C)
