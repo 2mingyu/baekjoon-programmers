2508ca83c7096dea2f67d3f284916dac9868527a
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-02-15 14:51:11 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    h=[[i for i in range(n+1)] for _ in range(k+1)]
+    for a in range(1,k+1):
+        for b in range(1,n+1):
+            h[a][b] = sum(h[a-1][i] for i in range(b+1))
+    print(h[k][n])
