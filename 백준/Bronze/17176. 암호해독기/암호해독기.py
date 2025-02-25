f1711ece446bc6161a3d6383b3e02849a37fbb35
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-02-25 23:12:05 +0900

    [Bronze I] Title: 암호해독기, Time: 224 ms, Memory: 38072 KB -BaekjoonHub

---
+++ 백준/Bronze/17176. 암호해독기/암호해독기.py
@@ -0,0 +1,11 @@
+N = int(input())
+l = sorted(list(map(int, input().split())))
+s = ''.join(sorted(input()))
+c = ''
+for i in l:
+    if i > 26:
+        c += chr(i - 27 + 97)
+    elif i:
+        c += chr(i - 1 + 65)
+    else: c += ' '
+print('ny'[s==c])
