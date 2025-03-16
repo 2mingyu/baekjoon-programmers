e6812918161cc3bf8e589f018c64daccf05d9a8a
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-03-16 18:14:08 +0900

    [Bronze IV] Title: H4x0r, Time: 652 ms, Memory: 34412 KB -BaekjoonHub

---
+++ 백준/Bronze/26768. H4x0r/H4x0r.py
@@ -0,0 +1,9 @@
+for x in input():
+    y = ''
+    if x == 'a': y = 4
+    elif x == 'e': y = 3
+    elif x == 'i': y = 1
+    elif x == 'o': y = 0
+    elif x == 's': y = 5
+    else: y = x
+    print(y, end='')
