653baecb74f8db9f0fb479d39275bffc2f6ef2a1
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-03-26 20:05:21 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.py
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.py
@@ -1,9 +1,5 @@
 N = int(input())
 l = sorted([list(map(int, input().split())) for _ in range(N)])
 t = 0
-for e in l:
-    if e[0] < t:
-        t = t + e[1]
-    else:
-        t = e[0] + e[1]
+for e in l: t = max(t, e[0]) + e[1]
 print(t)
