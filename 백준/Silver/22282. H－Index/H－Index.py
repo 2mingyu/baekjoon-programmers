a6d58a58af89d96fdc8738a7c6ba2fe381903a08
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-04-18 16:57:15 +0900

    [Silver V] Title: H-Index, Time: 100 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Silver/22282. H－Index/H－Index.py
+++ 백준/Silver/22282. H－Index/H－Index.py
@@ -2,9 +2,7 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-c = sorted([int(input()) for _ in range(n)], reverse=True)
+c = sorted([int(input()) for _ in range(n)])
 H = 0
-for e in c:
-    if e >= H+1: H += 1
-    else: break
+while c and c.pop() >= H+1: H += 1
 print(H)
