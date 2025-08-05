1b65bd1aa2b092393ebd47efa143d059ce8870f2
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-08-05 17:33:46 +0900

    [Silver V] Title: 행렬 곱셈, Time: 180 ms, Memory: 33564 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+M, K = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(M)]
+C = [[0]*K for _ in range(N)]
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            C[n][k] += A[n][m] * B[m][k]
+for n in range(N): print(*C[n])
