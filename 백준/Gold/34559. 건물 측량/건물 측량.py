b24aec9bc5b18e809801637f4b7100a21142e365
2mingyu <76238096+2mingyu@users.noreply.github.com>
2025-10-20 00:02:10 +0900

    [Gold IV] Title: 건물 측량, Time: 2248 ms, Memory: 98672 KB -BaekjoonHub

--- 백준/Gold/34559. 건물 측량/건물 측량.py
+++ 백준/Gold/34559. 건물 측량/건물 측량.py
@@ -1,22 +1,12 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-N, M = map(int, input().split())
-m = [list(input()) for _ in range(N)]
-q = deque([(0, 0)])
+I=open(0).readline
+N,M=map(int,I().split())
+m=[list(I())for _ in range(N)]
+q=[(0,0)]
 while q:
-    y, x = q.popleft()
-    for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-        ny, nx = y+dy, x+dx
-        if 0 <= ny < N and 0 <= nx < M and m[ny][nx] == '0':
-            m[ny][nx] = '2'
-            q.append((ny, nx))
-s = [[0]*(M+1) for _ in range(N+1)]
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + (m[i-1][j-1] != '2')
-Q = int(input())
-for _ in range(Q):
-    r1, c1, r2, c2 = map(int, input().split())
-    t = s[r2][c2] - s[r1-1][c2] - s[r2][c1-1] + s[r1-1][c1-1]
-    print(f'No {t}' if t else 'Yes')
+ y,x=q.pop()
+ for a,b in((y-1,x),(y+1,x),(y,x-1),(y,x+1)):
+  if 0<=a<N and 0<=b<M and m[a][b]=='0':m[a][b]='2';q+=[(a,b)]
+s=[[0]*(M+1)for _ in range(N+1)]
+for i in range(N):
+ for j in range(M):s[i+1][j+1]=s[i][j+1]+s[i+1][j]-s[i][j]+(m[i][j]!='2')
+for _ in'0'*int(I()):a,b,c,d=map(int,I().split());t=s[c][d]-s[a-1][d]-s[c][b-1]+s[a-1][b-1];print(('Yes','No %d'%t)[t>0])
