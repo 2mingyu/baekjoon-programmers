89010c4112b849da35bfc4703e72430562b47fac
2mingyu <76238096+2mingyu@users.noreply.github.com>
2026-03-26 00:55:26 +0900

    [Bronze III] Title: 17배, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5893. 17배/17배.py
+++ 백준/Bronze/5893. 17배/17배.py
@@ -1,18 +1,2 @@
-a = input()
-b = a + '0000'
-
-i = len(a) - 1
-j = len(b) - 1
-c = 0
-r = []
-
-while i >= 0 or j >= 0 or c:
-    x = int(a[i]) if i >= 0 else 0
-    y = int(b[j]) if j >= 0 else 0
-    s = x + y + c
-    r.append(str(s % 2))
-    c = s // 2
-    i -= 1
-    j -= 1
-
-print(''.join(r[::-1]))
+a = int(input(), 2)
+print(f'{a*17:b}')
